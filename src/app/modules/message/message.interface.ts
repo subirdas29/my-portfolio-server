816134e3d3e4566f6622e3aa3243129e081e8a72
subircodealign <subir.codealign@gmail.com>
2026-06-02 16:12:09 +0600

    updated

--- src/app/modules/message/message.interface.ts
+++ src/app/modules/message/message.interface.ts
@@ -1,7 +1,7 @@
 export type TMessage = {
   name: string;
   email: string;
-  phone: string;
+  phone?: string;
   subject: string;
   message: string;
   status: 'Pending' | 'Replied' | 'No Response' | 'Dealing' | 'Booked' | 'Closed';
--- src/app/modules/message/message.model.ts
+++ src/app/modules/message/message.model.ts
@@ -5,7 +5,7 @@ const MessageSchema = new Schema<TMessage>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
-    phone: { type: String, required: true },
+    phone: { type: String, required: false, default: '' },
     subject: { type: String, required: true },
     message: { type: String, required: true },
     status: {
